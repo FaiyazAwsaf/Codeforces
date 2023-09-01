979be12e2d9a3de4b193908b0c62af7883f12ce1
Faiyaz A Awsaf <64351054+FaiyazAwsaf@users.noreply.github.com>
2023-09-02 00:03:12 +0600

    Create 4A.c

---
+++ A-Type/4A.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ 
+int main(void){
+ 
+    int w;
+ 
+    scanf("%d", &w);
+ 
+    if (w % 2 == 0 && w > 2 && w <= 100) {
+ 
+        printf("YES");
+        return 0;
+    }
+  
+    printf("NO");
+    return 0; 
+}
